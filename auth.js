3eafb8500161ca498bd1ec5cc7051e271f39f72d
phanem170-lgtm <phanem170@gmail.com>
2026-09-21 13:37:51 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,11 @@
+function login(username, password) {
+    if (username === 'admin' && password === '123') {
+        return true;
+    }
+    if (!username || username.trim() === '') {
+        throw new Error('Tài khoản không được để trống');
+    }
+    return false;
+}
+
+module.exports = login;
